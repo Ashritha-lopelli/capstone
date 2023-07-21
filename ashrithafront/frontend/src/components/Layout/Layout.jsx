788a1b53cd57d5c8df1e8a35f0631bfd4dd3f312
Ashritha9@ <ashrithalopelli@gmail.com>
2023-07-21 16:36:55 -0700

    add chatbot to layout component

--- ashrithafront/frontend/src/components/Layout/Layout.jsx
+++ ashrithafront/frontend/src/components/Layout/Layout.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './../Header/Header';
 import Routers from '../../router/Routers';
 import Footer from './../Footer/Footer';
-
+import ChatbotComponent from '../Chatbot/ChatbotComponent';
 const Layout = () => {
   return (
     <>
       <Header/>
       <Routers/>
       <Footer/>
+      <ChatbotComponent />
     </>
   )
 }
